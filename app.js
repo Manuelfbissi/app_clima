fc2a845befc040b3e31837567f44d9cfcc3ac413
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2023-06-05 23:58:11 -0300

    add map and deletediv

--- app.js
+++ app.js
@@ -25,27 +25,71 @@ form.addEventListener("submit", (event) => {
   
         // Mostrar información climática
         const weatherHTML = `
-          <p>Temperatura maxima: ${temp_max} °C</p>
-          <p>Temperatura minima: ${temp_min} °C</p>
-          <p>Humedad: ${humidity}%</p>
-          <p>Sensación termica: ${feels_like} °C</p>
-          <p>Presion atmosferica: ${pressure} hPa</p>
-          <p>Velocidad de viento: ${speed} km/h</p>
-          
-        `;
+      <div class="cubos mb-2">
+        <p>Temperatura maxima: ${temp_max} °C</p>
+        </div>
+        <div class="cubos mb-2">
+        <p>Temperatura minima: ${temp_min} °C</p>
+        </div>
+        <div class="cubos mb-2">
+        <p>Humedad: ${humidity}%</p>
+        </div>
+        <div class="cubos mb-2">
+        <p>Sensación termica: ${feels_like} °C</p>
+        </div>
+        <div class="cubos mb-2">
+        <p>Presion atmosferica: ${pressure} hPa</p>
+        </div>
+        <div class="cubos mb-2">
+        <p>Velocidad de viento: ${speed} km/h</p>
+        </div>
+        
+      `;
         weatherDiv.innerHTML = weatherHTML;
   
         // Mostrar gráfico del clima
         const chartHTML = `<img src="https://openweathermap.org/img/w/${icon}.png" alt="${main}">`;
         chartDiv.innerHTML = chartHTML;
+
+        const borrarDiv = document.querySelector(".borrar");
+        borrarDiv.innerHTML = "";
   
         
       })
+      
   
       .catch((error) => {
         weatherDiv.textContent = "No se pudo obtener información climática";
       });
   });
+
+
+  //traigo el mapa 
+const apiKey = "qlV8LVWiQOvvVjpNBj4GxGumHU4yK3sz";
+const mapDiv = document.getElementById("map");
+const width = 400;
+const height = 300;
+const zoom = 8;
+const ciudadguradado = document.querySelector("#ciudad");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const location = form.elements.location.value;
+  const mapUrl = `https://www.mapquestapi.com/staticmap/v5/map?key=${apiKey}&center=${encodeURIComponent(location)}&size=${width},${height}&zoom=${zoom}`;
+
+  
+  mapDiv.innerHTML = `<img  src="${mapUrl}" alt="Mapa de ${location}">`;
+  console.log (mapDiv)
+
+  const ciudadHTML = `
+        <p>EL DESTINO ELEGIDO ES: ${location}</p>
+        
+        
+      `;
+      ciudadguradado.innerHTML = ciudadHTML;
+});
   
   
   
